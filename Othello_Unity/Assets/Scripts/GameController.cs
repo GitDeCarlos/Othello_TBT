5201470ec6e1e7ed0d60c88dfba4f05ef9adbe79
Mystaex <ethanchase14@gmail.com>
2021-01-15 20:20:26 -0600

    Place, Flip, and tileOnTurn functions
    Also added comments

--- Othello_Unity/Assets/Scripts/GameController.cs
+++ Othello_Unity/Assets/Scripts/GameController.cs
@@ -9,14 +9,16 @@ public class GameController : MonoBehaviour
     public GameObject p1_tile;
     public GameObject p2_tile;
     public GameObject[,] gameboard = new GameObject[8,8];
+    
     int turn = 0;
 
     // Start is called before the first frame update
     void Start(){
-        //Board is in [y,x] format
 
+        //Board is in [y,x] format
         Instantiate(board, new Vector3(5,5,0), Quaternion.identity);
         
+        //Creating the board
         for(int y = 0; y < 8; y++){
             for(int x = 0; x < 8; x++){
                 float pos_y = ((y+1)*0.1f) + y + 1;
@@ -25,6 +27,8 @@ public class GameController : MonoBehaviour
             }
         }
 
+
+        //Placing the starting 4 pieces 
         Destroy(gameboard[4,4]);
         gameboard[4,4] = Instantiate(p1_tile, new Vector3(5.5f, 5.5f, -0.5f), Quaternion.identity);
         Destroy(gameboard[3,3]);
@@ -32,7 +36,9 @@ public class GameController : MonoBehaviour
         Destroy(gameboard[3,4]);
         gameboard[3,4] = Instantiate(p2_tile, new Vector3(4.4f, 5.5f, -0.5f), Quaternion.identity);
         Destroy(gameboard[4,3]);
-        gameboard[4,3] = Instantiate(p2_tile, new Vector3(5.5f, 4.4f, -0.5f), Quaternion.identity);     
+        gameboard[4,3] = Instantiate(p2_tile, new Vector3(5.5f, 4.4f, -0.5f), Quaternion.identity);
+
+        gameboard[2,2] = flip(gameboard[2,2]);
     }
 
     // Update is called once per frame
@@ -40,9 +46,10 @@ public class GameController : MonoBehaviour
         
     }
 
-    //Flips a tile and returns new gameobject to reference
-    GameObject flip(GameObject curr){
+    GameObject tileOnTurn(){
         GameObject newTile;
+
+        //Determines which tile to place
         if(turn%2 == 0){
             newTile = p1_tile;
         }
@@ -50,10 +57,28 @@ public class GameController : MonoBehaviour
             newTile = p2_tile;
         }
 
+        return newTile;
+    }
+
+    GameObject place(GameObject curr){
+        GameObject newTile = tileOnTurn();
+
+        //Replaces the original tile with the new tile
         Vector3 pos = curr.transform.position;
-        Debug.Log("pos: " + pos);
         Destroy(curr);
         GameObject temp = Instantiate(newTile, pos, Quaternion.identity);
         return temp;
+    }
+
+    //Flips a tile and returns new gameobject to reference
+    //Note- is flip needed? With how place works, it may not be.
+    GameObject flip(GameObject curr){
+        GameObject newTile;
+        newTile = tileOnTurn();
+
+        if(curr != null){
+            GameObject temp = place(curr);
+            return curr;
+        }
     }
 }
