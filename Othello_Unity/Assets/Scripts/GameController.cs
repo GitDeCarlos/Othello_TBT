1cc542f7c7c479deacac8e5a360c79d775a394ff
Robert Fabbro <62273426+PolskaFly@users.noreply.github.com>
2021-01-22 20:12:41 -0600

    maybe

--- Othello_Unity/Assets/Scripts/GameController.cs
+++ Othello_Unity/Assets/Scripts/GameController.cs
@@ -2,6 +2,11 @@
 using System.Collections.Generic;
 using UnityEngine;
 
+public enum Player
+{
+    W,
+    B
+}
 public class GameController : MonoBehaviour
 {
     public GameObject buttonPre;
@@ -11,6 +16,14 @@ public class GameController : MonoBehaviour
     public GameObject[,] gameboard = new GameObject[8,8];
     
     int turn = 0;
+    public List<int> availableFields = new List<int>(64);
+
+    public OthelloAgent Agent1;
+    public OthelloAgent Agent2;
+
+    public bool PlayerChanged = true;
+    public Player currentPlayer;
+    public Player player;
 
     // Start is called before the first frame update
     void Start(){
@@ -18,32 +31,118 @@ public class GameController : MonoBehaviour
         //Board is in [y,x] format
         Instantiate(board, new Vector3(5,5,0), Quaternion.identity);
         
-        //Creating the board
+        PlayerChanged = true;
+
+        Instantiate(board, new Vector3(5,5,0), Quaternion.identity);
+        int counter = 0;
+
         for(int y = 0; y < 8; y++){
-            for(int x = 0; x < 8; x++){
-                float pos_y = ((y+1)*0.1f) + y + 1;
-                float pos_x = ((x+1)*0.1f) + x + 1;
-                gameboard[y,x] = Instantiate(buttonPre, new Vector3(pos_x,pos_y, -0.5f), Quaternion.identity);
+            for (int x = 0; x < 8; x++)
+            {
+                float pos_y = ((y + 1) * 0.1f) + y + 1;
+                float pos_x = ((x + 1) * 0.1f) + x + 1;
+                gameboard[y, x] = Instantiate(buttonPre, new Vector3(pos_x, pos_y, -0.5f), Quaternion.identity);
+                availableFields.Add(counter);
+                counter++;
             }
         }
 
 
         //Placing the starting 4 pieces 
         Destroy(gameboard[4,4]);
+        RemoveAvailable(availableFields, 4, 4);
         gameboard[4,4] = Instantiate(p1_tile, new Vector3(5.5f, 5.5f, -0.5f), Quaternion.identity);
         Destroy(gameboard[3,3]);
+        RemoveAvailable(availableFields, 3, 3);
         gameboard[3,3] = Instantiate(p1_tile, new Vector3(4.4f, 4.4f, -0.5f), Quaternion.identity);
         Destroy(gameboard[3,4]);
+        RemoveAvailable(availableFields, 3, 4);
         gameboard[3,4] = Instantiate(p2_tile, new Vector3(4.4f, 5.5f, -0.5f), Quaternion.identity);
         Destroy(gameboard[4,3]);
         gameboard[4,3] = Instantiate(p2_tile, new Vector3(5.5f, 4.4f, -0.5f), Quaternion.identity);
 
         gameboard[2,2] = flip(gameboard[2,2]);
+        RemoveAvailable(availableFields, 4, 3);
+        gameboard[4,3] = Instantiate(p2_tile, new Vector3(5.5f, 4.4f, -0.5f), Quaternion.identity);
     }
 
     // Update is called once per frame
-    void Update(){
-        
+    private void FixedUpdate()
+    {
+        if (PlayerChanged)
+        {
+            PlayerChanged = false;
+            GetCurrentAgent().RequestDecision();
+        }
+    }
+
+    public void ChangePlayer()
+    {
+            currentPlayer = currentPlayer == Player.B ? Player.W : Player.B;
+            PlayerChanged = true;
+    }
+    
+
+    private OthelloAgent GetCurrentAgent()
+    {
+        if (currentPlayer == Agent1.type) return Agent1;
+        else return Agent2;
+    }
+
+    private OthelloAgent GetAgentOfType(Player type)
+    {
+        if (Agent1.type == type) return Agent1;
+        else return Agent2;
+    }
+    private List<int> RemoveAvailable(List<int> available, int x, int y)
+    {
+        int count = 0;
+       for (int i = 0; i < 8; i++)
+        {
+            for (int j = 0; j < 8; j++)
+            {
+                if (y == i && x == j)
+                {
+                    available.RemoveAt(count);
+                }
+                count++;
+            }
+        }
+
+        return available;
+    }
+    /*
+    public void AssignRewardsOnWin(WinState winState)
+    {
+        if (winState == WinState.CrossWin)
+        {
+            GetAgentOfType(Player.X).SetReward(1f);
+            GetAgentOfType(Player.O).SetReward(-1f);
+        }
+        else if (winState == WinState.NoughtWin)
+        {
+            GetAgentOfType(Player.O).SetReward(1f);
+            GetAgentOfType(Player.X).SetReward(-1f);
+        }
+        else if (winState == WinState.Draw)
+        {
+            if (_board._fieldsOccupiedO > _board._fieldsOccupiedX)
+            {
+                GetAgentOfType(Player.O).SetReward(-0.25f);
+                GetAgentOfType(Player.X).SetReward(0.75f);
+            }
+            else
+            {
+                GetAgentOfType(Player.O).SetReward(0.75f);
+                GetAgentOfType(Player.X).SetReward(-0.25f);
+            }
+        }
+    }
+    */
+
+    public IEnumerable<int> GetAvailableFields()
+    {
+        return availableFields.ToArray();
     }
 
     GameObject tileOnTurn(){
@@ -84,3 +183,4 @@ public class GameController : MonoBehaviour
         return null;
     }
 }
+
---
+++ Othello_Unity/Assets/Scripts/OthelloAgent.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using Unity.MLAgents;
+using Unity.MLAgents.Policies;
+using Unity.MLAgents.Sensors;
+using UnityEngine;
+using UnityEngine.Serialization;
+
+public enum HeuristicMethod
+{
+    Random,
+    MinMax
+}
+
+public class OthelloAgent : Agent
+{
+    public Player type;
+    public HeuristicMethod heuristicMethod;
+    private GameController manager;
+
+    public override void OnEpisodeBegin()
+    {
+        type = GetComponent<BehaviorParameters>().TeamId == 0 ? Player.B : Player.W;
+    }
+
+    public override void Heuristic(float[] actionsOut)
+    {
+        var availableOptions = (int[])manager.GetAvailableFields();
+
+        if (heuristicMethod == HeuristicMethod.Random)
+        {
+            int randomField = availableOptions[Random.Range(0, availableOptions.Length)];
+            actionsOut[0] = randomField;
+        }
+    }
+
+    public override void CollectObservations(VectorSensor sensor)
+    {
+        foreach (var field in board.Fields)
+        {
+            sensor.AddOneHotObservation(field.ObserveField(type), 3);
+        }
+    }
+
+    public override void OnActionReceived(float[] vectorAction)
+    {
+        board.SelectField(Mathf.FloorToInt(vectorAction[0]), type);
+    }
+
+    public override void CollectDiscreteActionMasks(DiscreteActionMasker actionMasker)
+    {
+        actionMasker.SetMask(0, board.GetOccupiedFields());
+    }
+}
